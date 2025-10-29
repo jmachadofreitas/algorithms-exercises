51caae6e7e669368ea9b38e355eca189a7f415f5
João Machado de Freitas <jmachadofreitas@vivaldi.net>
2025-10-29 06:38:38 +0000

    Add bubbleSort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -10,14 +10,39 @@
   index's. Continue looping through until all values are in ascending order
 */
 
+
 function bubbleSort(nums) {
-  // code goes here
+  // Copy function's body to src/Sort.js and uncomment snapshot(nums)
+  // Run with npm start
+  let swapped = true; // sentinel variable
+  for (let it = 0; swapped; it++) {
+    swapped = false;
+    // snapshot(nums);
+    for (let i = 0; i < nums.length - 1 - it; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const tmp = nums[i];
+        nums[i] = nums[i+1];
+        nums[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  }
+  // snapshot(nums);
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
-  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-  const sortedNums = bubbleSort(nums);
-  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+test("bubble sort", function () {
+  const t0 = [];
+  bubbleSort(t0);
+  expect(t0).toEqual([]);
+  const t1 = [1];
+  bubbleSort(t1);
+  expect(t1).toEqual([1]);
+  const t2 = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  bubbleSort(t2);
+  expect(t2).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const t3 = [3,2,1];
+  bubbleSort(t3);
+  expect(t3).toEqual([1,2,3]);
 });
