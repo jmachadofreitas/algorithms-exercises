a036a7f007a82252906a4388c2d40b3e53e66216
João Machado de Freitas <jmachadofreitas@vivaldi.net>
2025-10-29 07:43:01 +0100

    Visualize bubbleSort

--- src/Sort.js
+++ src/Sort.js
@@ -4,13 +4,30 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 
 import "./sort.css";
 
-function sort(array) {
-  // do cool stuff here
-
+function sort(nums) {
   // call snapshot any time you do anything to the array
   // it's okay if you call it with duplicate value array,
   // it will deduplicate for you
-  snapshot(array);
+  // snapshot(nums);
+
+  // bubblesort
+  let swapped = true; // sentinel variable
+  for (let it = 0; swapped; it++) {
+    swapped = false;
+    snapshot(nums);
+    for (let i = 0; i < nums.length - 1 - it; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const tmp = nums[i];
+        nums[i] = nums[i+1];
+        nums[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  }
+  snapshot(nums);
+
+  // insertSort
+
 }
 
 export default function SortComponent() {
