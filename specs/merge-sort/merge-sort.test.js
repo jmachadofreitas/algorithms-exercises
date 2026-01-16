24366844815be52c7a0ac78c3d979dce925e88da
João Machado de Freitas <jmachadofreitas@vivaldi.net>
2026-01-16 15:05:31 +0000

    Add merge sorte solution

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,14 +6,43 @@
   You'll need to write more than just one function
 */
 
+function merge(left, right) {
+  // Simple implementation without indexes
+  let res = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      // shift: dequeu or pop from front 
+      res.push(left.shift());
+    } else {
+      res.push(right.shift());
+    }
+  }
+  return res.concat(left, right);
+}
+
 const mergeSort = (nums) => {
-  // code goes here
+  if (nums.length <= 1) {
+    return nums;
+  }
+  const mid = Math.floor(nums.length / 2);
+  const left = nums.slice(0, mid);
+  const right = nums.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
-  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-  const ans = mergeSort(nums);
+test("merge sort", function () {
+  let nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  let ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  nums = [];
+  ans = mergeSort(nums);
+  expect(ans).toEqual([]);
+  nums = [10];
+  ans = mergeSort(nums);
+  expect(ans).toEqual([10]);
+  nums = [10, 5];
+  ans = mergeSort(nums);
+  expect(ans).toEqual([5, 10]);
 });
